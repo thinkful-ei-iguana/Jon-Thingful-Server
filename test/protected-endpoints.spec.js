92431421e90f10f39d7841cbbd8b0744f6ba2259
Jon <jonathanjackson424@gmail.com>
2019-12-19 15:59:41 -0500

    added secure login

--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -2,14 +2,14 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe('Protected endpoints', function() {
+describe('Protected endpoints', function () {
   let db
 
   const {
     testUsers,
-    testArticles,
-    testComments,
-  } = helpers.makeArticlesFixtures()
+    testThings,
+    testReviews,
+  } = helpers.makeThingsFixtures()
 
   before('make knex instance', () => {
     db = knex({
@@ -25,58 +25,52 @@ describe('Protected endpoints', function() {
 
   afterEach('cleanup', () => helpers.cleanTables(db))
 
-  beforeEach('insert articles', () =>
-    helpers.seedArticlesTables(
+  beforeEach('insert things', () =>
+    helpers.seedThingsTables(
       db,
       testUsers,
-      testArticles,
-      testComments,
+      testThings,
+      testReviews,
     )
   )
 
   const protectedEndpoints = [
     {
-      name: 'GET /api/articles/:article_id',
-      path: '/api/articles/1',
+      name: 'GET /api/things/:thing_id',
+      path: '/api/things/1',
       method: supertest(app).get,
     },
     {
-      name: 'GET /api/articles/:article_id/comments',
-      path: '/api/articles/1/comments',
+      name: 'GET /api/things/:thing_id/reviews',
+      path: '/api/things/1/reviews',
       method: supertest(app).get,
     },
     {
-      name: 'POST /api/comments',
-      path: '/api/comments',
+      name: 'POST /api/reviews',
+      path: '/api/reviews',
       method: supertest(app).post,
     },
   ]
 
   protectedEndpoints.forEach(endpoint => {
     describe(endpoint.name, () => {
-      it(`responds 401 'Missing basic token' when no basic token`, () => {
+      it(`responds 401 'Missing bearer token' when no bearer token`, () => {
         return endpoint.method(endpoint.path)
-          .expect(401, { error: `Missing basic token` })
+          .expect(401, { error: `Missing bearer token` })
       })
 
-      it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
-        const userNoCreds = { user_name: '', password: '' }
+      it(`responds 401 'Unauthorized request' when invalid JWT secret`, () => {
+        const validUser = testUsers[0]
+        const invalidSecret = 'bad-secret'
         return endpoint.method(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(userNoCreds))
+          .set('Authorization', helpers.makeAuthHeader(validUser, invalidSecret))
           .expect(401, { error: `Unauthorized request` })
       })
 
-      it(`responds 401 'Unauthorized request' when invalid user`, () => {
-        const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
+      it(`responds 401 'Unauthorized request' when invalid sub in payload`, () => {
+        const invalidUser = { user_name: 'user-not-existy', id: 1 }
         return endpoint.method(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
-          .expect(401, { error: `Unauthorized request` })
-      })
-
-      it(`responds 401 'Unauthorized request' when invalid password`, () => {
-        const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
-        return endpoint.method(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
+        .set('Authorization', helpers.makeAuthHeader(invalidUser))
           .expect(401, { error: `Unauthorized request` })
       })
     })
